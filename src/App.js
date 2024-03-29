806b943657ac7b2a1ed2859acf01fac2aba5a803
Edme - Dawn <nicmaver1337@gmail.com>
2024-03-29 18:14:22 +1000

    create: Tailwind and React Project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container bg-red-500 my-4">
+      <p className="size-10 text-cyan-800">SCREAMS</p>
+    </div>
+  );
+}
+
+export default App;
