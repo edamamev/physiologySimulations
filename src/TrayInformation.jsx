6c77f8770f4803bd2a2b9db586d461013d74e602
Edme - Dawn <nicmaver1337@gmail.com>
2024-03-29 19:45:44 +1000

    feat: UI Skeleton Added

---
+++ src/TrayInformation.jsx
@@ -0,0 +1,7 @@
+export default function TrayInformation(){
+    return(
+        <div className="bg-yellow-400 h-96 absolute top-0 left-0 w-32 flex flex-row items-center justify-center">
+            <p>DATA</p>
+        </div>
+    )
+}
